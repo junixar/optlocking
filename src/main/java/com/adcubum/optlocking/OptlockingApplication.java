9e069325421ded7d44d71393187584eed7d1ceba
yury.yudin <yury.yudin@adcubum.com>
2019-09-16 15:00:49 +0200

    F Initial spring+jpa application.

---
+++ src/main/java/com/adcubum/optlocking/OptlockingApplication.java
@@ -0,0 +1,13 @@
+package com.adcubum.optlocking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptlockingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptlockingApplication.class, args);
+	}
+
+}
