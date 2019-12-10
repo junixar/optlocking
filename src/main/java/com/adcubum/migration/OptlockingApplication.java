e141840934171e9cd77be742ebe2537d5c57914a
Stefanie Mangin <stefanie.mangin@adcubum.com>
2019-12-10 09:08:38 +0100

    [SYRIUS-77552] close of SpringBoot after run for convenience

--- src/main/java/com/adcubum/migration/OptlockingApplication.java
+++ src/main/java/com/adcubum/migration/OptlockingApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OptlockingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OptlockingApplication.class, args);
+		SpringApplication.run(OptlockingApplication.class, args).close();
 	}
 
 }
