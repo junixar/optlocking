7aef63f7f18565f011b782cdddd9c3883b2d9291
Stefanie Mangin <stefanie.mangin@adcubum.com>
2020-01-16 11:27:52 +0100

    adapt DMLs

--- src/main/resources/db/migration/test_parallelDML.sql
+++ src/main/resources/db/migration/test_parallelDML.sql
@@ -4,9 +4,10 @@
 ALTER SESSION ENABLE PARALLEL DML;
 
 --changeset bob:4
-INSERT INTO syrius.e VALUES ('bla', 'myeamil'); 
+SELECT count(*) FROM syrius.e; 
 INSERT INTO syrius.e VALUES ('blub', 'myeamil'); 
 INSERT INTO syrius.e VALUES ('bling', 'myeamil'); 
+SELECT count(*) FROM syrius.e; 
 
 --changeset bob:5
 ALTER SESSION DISABLE PARALLEL DML;
